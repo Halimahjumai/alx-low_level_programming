cd30f727d212f32c70964afc062d438b0808225a
Halimahjumai <adedoyinhalimatjumai@gmail.com>
2023-02-17 14:44:12 -0800

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,16 +4,12 @@
 
 /**
  * main - Entry point
- * void: When value X is void, it means not accepting any arguement
- (* Description- Prints out the last part of a quote in the standard error.)?
  *
-
- (unistd header: has the function write)
  * Return: 1 only (success)
  */
 int main(void)
 {
 
-	write(2, "and that piece of art is useful\"- Dora Korpar,2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
