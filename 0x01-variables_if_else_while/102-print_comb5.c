eed23f78f2c3d95a55060b7c3942bc0ccaad9994
Halimahjumai <adedoyinhalimatjumai@gmail.com>
2023-02-20 14:54:43 -0800

    two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,29 +8,27 @@
 
 int main(void)
 {
-
 	int a;
 	int b;
 
-	for (a = 0 ; a < 100 ; a++)
+	for (a = 0 ; a <= 98 ; a++)
 	{
-		for (b = 0 ; b < 100 ; b++)
+		for (b = 1 ; b <= 99 ; b++)
 		{
-			if (a < b)
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a + a != 17 && b + b != 18)
 			{
-				putchar((a / 10) + 48);
-				putchar((a % 10) + 48);
+				putchar(',');
 				putchar(' ');
-				putchar((b / 10) + 48);
-				putchar((b % 10) + 48);
-				if (a != 98 && b != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
 	putchar('\n');
 	return (0);
+
+
 }
