ad914cb8bf26ac5cebb3e8a073746fc787a5adc2
Halimahjumai <adedoyinhalimatjumai@gmail.com>
2023-02-21 15:53:00 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,8 @@
 
 /**
  * print_sign - this function prints signs
- *
- * @n: function that prints parameters
- *
- * Return: 1, 0 and -1
+ * @n: function parameters
+ * Return: 0, 1 and -1
  */
 
 int print_sign(int n)
@@ -13,22 +11,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
+	_putchar(',');
+	_putchar(' ');
 }
