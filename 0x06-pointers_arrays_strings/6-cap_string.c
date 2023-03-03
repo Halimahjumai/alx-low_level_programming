212fbc495970867755eb693f548bdf4b446d7788
Halimahjumai <adedoyinhalimatjumai@gmail.com>
2023-03-03 08:19:03 -0800

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,22 @@
 char *cap_string(char *s)
 {
 	int i = 0, j;
-
 	char a[] = " \t\n,;.!?\"(){}";
 
 	while (*(s + i))
 	{
-		if (*(s + 1) >= 'a' && *(s + i) <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-
-		if (i == 0)
-			*(s + i) -= 'a' - 'A';
-		else
-		{
-			for (j = 0; j <= 12; j++)
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				if (a[j] == *(s + i - 1))
-					*(s + i) -= 'a' - 'A';
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
-		}
-
 		}
 		i++;
 	}
