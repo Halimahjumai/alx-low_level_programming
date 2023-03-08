d8ef79b055817b3ef04f632aefabcc0f35b1abaa
Halimahjumai <adedoyinhalimatjumai@gmail.com>
2023-03-08 11:13:38 -0800

    length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - this function checks the length of a string
+ * @s: - function parameter
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+}
